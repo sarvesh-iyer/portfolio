e9181c406e03345a6ba3c889992e54f55fb58479
sarvesh-iyer <sarvesh.iyer3@gmail.com>
2024-07-13 16:24:22 +0530

    skills section completed - tech skills icons added

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './skills.css'
+import { Card } from '@mui/material'
+import { skillsList } from '../../data/skillsList'
 
 export const Skills = () => {
     return (
@@ -12,6 +14,24 @@ export const Skills = () => {
                     My Skills
                 </h3>
                 
+                <div className="skills-card-container flex flex-wrap gap-4   justify-center">
+                    {skillsList.map(skill => (
+                        <div 
+                            key={skill.id} 
+                            className="skill-card grid items-center"
+                        >
+                            <img 
+                                src={skill.img} 
+                                alt={skill.name} 
+                                className='mx-auto'
+                                width={65}
+                            />
+                            <span className="mt-3 text-center leading-6">
+                                {skill.name}
+                            </span>
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     )
