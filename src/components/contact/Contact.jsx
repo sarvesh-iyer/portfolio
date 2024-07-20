c07e6cec4bb550c34f233498e8e53ffd7b38de65
sarvesh-iyer <sarvesh.iyer3@gmail.com>
2024-07-20 12:13:04 +0530

    input field component created, contact form done, contact section UI completed

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,39 +1,68 @@
 import React from 'react'
 import { socialLinksList } from '../../data/socialLinkData'
 import './contact.css'
+import { InputField } from '../utils/input-field/InputField'
+import { ActionButton } from '../utils/button/ActionButton'
 
 export const Contact = () => {
     return (
         <section className="section-wrapper">
             <div className="section-container contact-container">
                 <div className="left-container">
-                    <h2>Let's Connect</h2>
-                    <p className='mb-2'>Say hello at <a 
-                            href='mailto:sarvesh.iyer3@gmail.com' 
-                            className="animated-underline transition"
-                        >
-                            sarvesh.iyer3@gmail.com
-                        </a>
-                    </p>
-                    <p>For more info, here's my <span className="animated-underline transition">
-                        resume
-                    </span></p>
-
-                    <div className="social-link-wrapper space-x-6 ">
-                        {socialLinksList.map(item => (
-                            <a
-                                key={item.id} 
-                                href={item.link}
-                                target='_blank'
-                                className="transition social-icons p-2"
+                    <div>
+                        <h2>Let's Connect</h2>
+                        <p className='mb-2'>Say hello at <a 
+                                href='mailto:sarvesh.iyer3@gmail.com' 
+                                className="animated-underline transition"
                             >
-                                <img src={item.icon} alt={item.name} />
+                                sarvesh.iyer3@gmail.com
                             </a>
-                        ))}
+                        </p>
+                        <p>For more info, here's my <span className="animated-underline transition">
+                            resume
+                        </span></p>
+
+                        <div className="social-link-wrapper space-x-6 ">
+                            {socialLinksList.map(item => (
+                                <a
+                                    key={item.id} 
+                                    href={item.link}
+                                    target='_blank'
+                                    className="transition social-icons p-2"
+                                >
+                                    <img src={item.icon} alt={item.name} />
+                                </a>
+                            ))}
+                        </div>
+                    </div>
+                    
+                    <div className="foot-note">
+                        © 2024 Sarvesh Iyer
                     </div>
                 </div>
                 <div className="right-container">
-                    
+                    <form className="space-y-5">
+                        <InputField 
+                            label="Name"
+                            name="name"
+                        />
+                        <InputField 
+                            label="Email"
+                            name="email"
+                        />
+                        <InputField 
+                            label="Subject"
+                            name="subject"
+                        />
+                        <InputField 
+                            label="Message"
+                            name="message"
+                            type="textarea"
+                        />
+                        <ActionButton>
+                            Submit
+                        </ActionButton>
+                    </form>
                 </div>
             </div>
         </section>
--- src/components/utils/input-field/InputField.jsx
+++ src/components/utils/input-field/InputField.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import './input_field.css'
 
-export const InputField = () => {
+export const InputField = (props) => {
+
+    let {
+        label,
+        type,
+        name,
+        value,
+        onChange,
+        inputRef,
+        placeholder,
+        isError
+    } = props
+
+    const handleOnChange = (e) => {
+        if(onChange) onChange(e)
+    } 
+
     return (
-        <div>
-            
+        <div className="input-field-wrapper">
+            <label 
+                htmlFor={name}
+                className="label-text"
+            >
+                {label}
+            </label>
+            {type !== 'textarea' ? (
+                <input 
+                    ref={inputRef}
+                    name={name}
+                    id={name}
+                    type={type}
+                    className={[
+                        "input-field",
+                        isError && "error-field"
+                    ].join(' ')}
+                    value={value}
+                    onChange={handleOnChange}
+                    placeholder={placeholder}
+                />
+            ) : (
+                <textarea 
+                    ref={inputRef}
+                    name={name} 
+                    id={name}
+                    className={[
+                        "input-field",
+                        isError && "error-field"
+                    ].join(' ')}
+                    value={value}
+                    onChange={handleOnChange}
+                    placeholder={placeholder}
+                    rows={6}
+                ></textarea>
+            )}
         </div>
     )
 }
