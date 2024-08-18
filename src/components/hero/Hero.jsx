6e99fc9a09ebaacc006206e4eb5eebbed05e7d24
sarvesh-iyer <sarvesh.iyer3@gmail.com>
2024-08-18 11:54:51 +0530

    hero responsive images with different sizes added

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,6 +7,12 @@ import { socialLinksList } from '../../data/socialLinkData'
 import NorthEastIcon from '@mui/icons-material/NorthEast';
 import { useNavigate } from 'react-router-dom'
 
+import pic1_b8srcr_c_scalew_180 from '../../assets/images/hero/responsive-hero/pic1_b8srcr_c_scale,w_180.webp' 
+import pic1_b8srcr_c_scalew_602 from '../../assets/images/hero/responsive-hero/pic1_b8srcr_c_scale,w_602.webp' 
+import pic1_b8srcr_c_scalew_924 from '../../assets/images/hero/responsive-hero/pic1_b8srcr_c_scale,w_924.webp' 
+import pic1_b8srcr_c_scalew_1205 from '../../assets/images/hero/responsive-hero/pic1_b8srcr_c_scale,w_1205.webp' 
+import pic1_b8srcr_c_scalew_1380 from '../../assets/images/hero/responsive-hero/pic1_b8srcr_c_scale,w_1380.webp' 
+
 export const Hero = () => {
     
     const navigate = useNavigate()
@@ -51,12 +57,25 @@ export const Hero = () => {
                 </div>
                 <div className="right-container">
                     <div className="image-container">
-                        <picture>
+                        {/* <picture>
                             <source srcSet={MyPicWebp} type="image/webp" />
                             <img 
                                 src={MyPic} alt="Sarvesh Iyer" 
                                 width="300" height="200"
                                 className='w-full h-full object-cover' 
+                            />
+                        </picture> */}
+                        <picture>
+                            <img
+                                sizes="(min-width: 1380px) 580px, (min-width: 940px) 42.38vw, (min-width: 660px) calc(45.77vw - 30px), 91.76vw"
+                                srcSet={`${pic1_b8srcr_c_scalew_180} 180w,
+                                    ${pic1_b8srcr_c_scalew_602} 602w,
+                                    ${pic1_b8srcr_c_scalew_924} 924w,
+                                    ${pic1_b8srcr_c_scalew_1205} 1205w,
+                                    ${pic1_b8srcr_c_scalew_1380} 1380w`}
+                                src={pic1_b8srcr_c_scalew_1380}
+                                alt="sarvesh iyer"
+                                className='w-full h-full object-cover'
                             />
                         </picture>
                     </div>
